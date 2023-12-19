2f3a3ef21ce902944f3252e8fbcd2df87d197a40
Mikhail <smirnow.mishas2017@gmail.com>
2023-12-19 18:52:38 +0600

    all_ready

--- src/app/my-machine-app/my-machine-app.component.ts
+++ src/app/my-machine-app/my-machine-app.component.ts
@@ -4,11 +4,13 @@ import { pipeline } from '@xenova/transformers';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../api.service';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-my-machine-app',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, FormsModule],
   templateUrl: './my-machine-app.component.html',
   styleUrl: './my-machine-app.component.css'
 })
@@ -16,25 +18,39 @@ import { HttpClientModule } from '@angular/common/http';
 export class MyMachineAppComponent {
   translation!: string;
   hero = 'Windstorm';
-  sentence = "I love you";
+  sentence: string = '';
   constructor(private http: HttpClient) { }
   message!: object;
   
-  translateText() {
-    const url = 'http://127.0.0.1:8000/translate';
-    this.http.get(url, {}).subscribe((response) => {
-      // Handle the response here
-      console.log(response);
-      this.message = response;
-    });
-  }
-
   sendSentence() {
-    const apiUrl = 'http://127.0.0.1:8000';
+    const apiUrl = 'http://127.0.0.1:8000/fr';
     this.http.post(apiUrl, { sentence: this.sentence }).subscribe(
-      (response) => {
+      (response: any) => {
+        this.translation = response;
         console.log(response);
       }
     );
   }
+  // sendSentence() {
+  //   const apiUrl = 'http://127.0.0.1:8000/fr';
+  //   this.http.post(apiUrl, { sentence: this.sentence }).subscribe(
+  //     (response) => {
+  //       console.log(response);
+  //     }
+  //   );
+  // }
 }
+// translateText() {
+      //   const url = 'http://127.0.0.1:8000/translate';
+      //   this.http.get(url, {}).subscribe((response) => {
+      //     // Handle the response here
+      //     console.log(response);
+      //     this.message = response;
+      //   });
+      // }
+
+      // this.http.get(apiUrl, {}).subscribe((response) => {
+        //   // Handle the response here
+        //   console.log(response);
+      //   this.message = response;
+      // });
